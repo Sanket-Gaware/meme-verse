44c51ae034697c62675802500c7008c66ee00fc6
Sanket Gaware <gawaresanket03@gmail.com>
2025-04-07 12:00:10 +0530

    'fix-auth-route'

--- src/Pages/MainWrapper.jsx
+++ src/Pages/MainWrapper.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Sidebar from "../Components/Sidebar";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function MainWrapper() {
   const location = useLocation();
+  const navigate = useNavigate();
   useEffect(() => {
     localStorage.setItem("LastVisitedPath", location.pathname);
   }, [location.pathname]);
+  useEffect(() => {
+    !localStorage.getItem("Token") ? navigate("/") : "";
+  }, []);
   return (
     <div className="grid grid-cols-1 md:grid-cols-12 w-full">
       <Sidebar />
