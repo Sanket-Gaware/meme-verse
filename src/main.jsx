32229d1f0163b5dcc8fba6f7605c8533ae84add5
Sanket Gaware <gawaresanket03@gmail.com>
2025-10-18 15:27:43 +0530

    store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import store, { persister } from "./Store/Store.js";
+import store, { persister } from "./Store/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
